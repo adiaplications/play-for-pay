224b597c0261128473e54d272b27d2c0e16d1053
Adi <adi@seekingalpha.com>
2014-06-06 02:19:51 +0300

    add OS column

--- db/migrate/20140422222114_create_campaigns.rb
+++ db/migrate/20140422222114_create_campaigns.rb
@@ -2,6 +2,7 @@ class CreateCampaigns < ActiveRecord::Migration
   def up
     create_table :campaigns do |t|
       t.string "company", :limit => 30, :default => "Mobpartner", :null => false
+      t.string "os" , :default => "Android", :null => false
       t.string "commision_type", :default => "CPI", :null => false
       t.string "name", :limit => 30, :default => "", :null => false
       t.string "image_url", :default => "", :null => false
