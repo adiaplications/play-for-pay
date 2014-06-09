430a6b60a3ef3f3af19cfeaf7a28c80adb3dbfdf
Adi <adi@seekingalpha.com>
2014-06-09 03:28:49 +0300

    create new server secret token #2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 Ads::Application.config.session_store :cookie_store, key: '_ads_session'
+Ads::Application.config.secret_key_base = 'a64c625942dd1292afa803fb36dd82f1438319ff3031edd5fea8c890fb26b3c1a66593562d97fec403c708b34d51ceb694d0493523a3bbd91de6b40b11923dec'
