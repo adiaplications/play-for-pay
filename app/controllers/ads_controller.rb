07f6473ecde2fa3727ccec17c590f5b3a6da3035
Adi <adi@seekingalpha.com>
2014-06-09 04:30:16 +0300

    exclude confirm login for action get ads by country

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -2,7 +2,7 @@ class AdsController < ApplicationController
 
   layout 'admin'
 
-  before_action :confirm_logged_in
+  before_action :confirm_logged_in, :except => [:show_ads_by_country]
 
   helper_method :sort_column, :sort_direction
 
